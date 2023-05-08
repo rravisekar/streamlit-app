e4886442128815066ef5a51cb5999a47953df884
rravisekar <53174447+rravisekar@users.noreply.github.com>
2023-05-08 14:08:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import snowflake.connector
 import streamlit as st
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(user = "rravisekar",
+                                     password = "Rachana33$",
+                                     account = "ID89449.ca-central-2.aws",
+                                     warehouse = "compute_wh",
+                                     database = "zenas_athleisure_db",
+                                     schema = "products")
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
