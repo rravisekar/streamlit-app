a87846ab4aaaae2541a6cefa07a064ddea3d3c4c
rravisekar <53174447+rravisekar@users.noreply.github.com>
2023-05-08 13:49:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# import snowflake.connector
+import snowflake.connector
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 # my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
